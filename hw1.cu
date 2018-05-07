80e4f05e1112d4de13a6f05a20bb31fc6b142e30
Alon <alontalmor@gmail.com>
2018-05-08 01:10:01 +0300

    Changed the sum of the hist from atomic-sum to reduce to improve efficiency.

--- hw1.cu
+++ hw1.cu
@@ -5,6 +5,7 @@
 
 #define IMG_DIMENSION 32
 #define N_IMG_PAIRS 10000
+#define N_IMG_PAIRS_POW2 10240
 #define HIST_SIZE 256
 #define SIMPLE 0
 #define SHARED 1
@@ -187,16 +188,51 @@ __global__ void image_to_histogram_large(uchar *image1, OUT int *hist1) {
     __syncthreads();
 }
 
+// a function to reduce the distances array. This function works on array of size 256 with 128 threads.
+// The param distances should be an array which is shared among the threads.
+__device__ void reduce(double *distances, int size) {
+    int tid = threadIdx.x;
+    int length = size / 2;
+    while (length >= 1) {
+        if (tid < length) {
+            distances[tid] = (double)distances[tid] + (double)distances[tid + length];
+        }
+        __syncthreads();
+        length /= 2;
+    }
+}
 // calling a __global__ function from a __global__ function is only allowed on the compute_35
 // architecture or above. So inorder to use it later I'll extract it's functionality to a
 // __device__ function.
 // this function calculates the distance between to images based on their histograms.
 __device__ void d_histogram_distance(int *hist1, int *hist2, OUT double *distance) {
     int tid = threadIdx.x;
+    // initialize an array to place the distances in, so later we can reduce it (sum its elements).
+    // it is important to use __shared__ here so all the threads in the same thread block share the
+    // same array.
+    __shared__ double distances [HIST_SIZE];
     // int bid = blockIdx.x;
+    // calculate the distance of each position in the histograms, if the sum of both images hist value
+    // is zero at the given position then the distance is also zero (this is reason for the use of if-else here). 
     if(hist1[tid] + hist2[tid] > 0) {
-        double add = ((double)SQR(hist1[tid] - hist2[tid])) / (hist1[tid] + hist2[tid]);
-        atomicAdd(distance, add);
+        distances[tid] = ((double)SQR(hist1[tid] - hist2[tid])) / (hist1[tid] + hist2[tid]);
+        // atomicAdd(distance, add);
+    } else {
+        distances[tid] = 0;
+    }
+    // wait for all the threads to complete calculating the distances
+    __syncthreads();
+    // perform a reduce using half of the threads. We use only have because each thread is summing up
+    // 2 elements in each iteration, which means that the sum requires log_2(HIST_SIZE) iterations.
+    // we could also use all the threads, and at least half of them would always be idle.
+    if (tid < HIST_SIZE / 2) {
+        reduce(distances, HIST_SIZE);
+    }
+    // wait for the reduce to complete and copy the result of the reduce to the OUT pointer.
+    __syncthreads();
+    // we can do it once using a single thread
+    if (tid == 0) {
+        *distance = distances[0];
     }
     __syncthreads();
 }
@@ -204,12 +240,47 @@ __global__ void histogram_distance(int *hist1, int *hist2, OUT double *distance)
     d_histogram_distance(hist1, hist2, distance);
 }
 
-__global__ void histogram_distance_large(int *hist1, int *hist2, OUT double *distance) {
+__global__ void histogram_distance_large(int *hist1, int *hist2, OUT double *distances) {
     // int tid = threadIdx.x;
     int bid = blockIdx.x;
     // calculate each pair distance, the histograms of each pair are placed in jumps of HIST_SIZE (256)
     // from one another. Each block-id  will handle a different pair. the result is saved in distance.
-    d_histogram_distance(&hist1[HIST_SIZE * bid], &hist2[HIST_SIZE * bid], distance);
+    d_histogram_distance(&hist1[HIST_SIZE * bid], &hist2[HIST_SIZE * bid], &distances[bid]);
+    __syncthreads();
+
+}
+
+__global__ void hist_reduce_large(double *distances, unsigned int *retirement_count, double OUT *distance) {
+    int tid = threadIdx.x;
+    int bid = blockIdx.x;
+    int offset = blockDim.x * 2; // offset value is 512*2=1024
+    // this variable will be used specify the last block passing the fence.
+    bool last = false;
+    // use the regular reduce this time in chunks of "offset".
+    reduce(&distances[bid * offset], offset);
+    __syncthreads();
+    // after all blocks finished calculation we'll have an array of all images distances,
+    // but we still need to reduce this array.
+    // first use fence so all the threads in all the thread-blocks share the most up-to-date
+    // distances array.
+    __threadfence();
+
+    // next, use tickets to find the last thread block and use its 0-thread to sum up the array.
+    // the last thread can sum up the array since we know that all the other threads passed the fence.
+    // this thread does not have many elements to sum (if i calculated correctly it should have only 
+    // (N_IMG_PAIRS_POW2 / offset) = (10240 / 1024) = 10 elements to count)
+    if (tid == 0) {
+        // inc the ticket for each block finished
+        unsigned int ticket = atomicInc(retirement_count, gridDim.x);
+        // check if the block is the last block to finish, and if so sum the 10 elements.
+        last = (ticket == gridDim.x - 1);
+        if (last) {
+            *distance = 0;
+            for (int i = 0; i < N_IMG_PAIRS_POW2; i += offset) {
+                *distance += distances[i];
+            }
+        }
+    }
     __syncthreads();
 }
 
@@ -272,10 +343,10 @@ double gpu_average_distance_calculator(uchar *images1, uchar *images2, int type)
         CUDA_CHECK( cudaFree(gpu_hist2) );
         CUDA_CHECK( cudaFree(gpu_hist_distance) );
     }     
-        CUDA_CHECK(cudaDeviceSynchronize());
-        double average_distance = total_distance / N_IMG_PAIRS;
-        printf("[%s version] GPU average distance: %f\n", (type == SIMPLE ? "SIMPLE" : "SHARED"), average_distance);
-        return average_distance;
+    CUDA_CHECK(cudaDeviceSynchronize());
+    double average_distance = total_distance / N_IMG_PAIRS;
+    printf("[%s version] GPU average distance: %f\n", (type == SIMPLE ? "SIMPLE" : "SHARED"), average_distance);
+    return average_distance;
 }
 
 double gpu_large_average_distance_calculator(uchar *images1, uchar *images2) {
@@ -283,12 +354,16 @@ double gpu_large_average_distance_calculator(uchar *images1, uchar *images2) {
     uchar *gpu_image1, *gpu_image2;
     // the histograms calculated on the gpu will be saved here
     int *gpu_hist1, *gpu_hist2;
-    // the distance calculated by the gpu in each iteration will be saved here
+    // the distances calculated by the gpu in each thread block will be saved here
+    double *gpu_hist_distances;
+    // the total ditance will be saed here
     double *gpu_hist_distance;
     // the distance calculated on the gpu will be copied back to this cpu variable.
     // this time it is also the total distance calculated (not the average) since
     // we are doing a single iteration (so we calculate everything "at once").
     double cpu_total_distance = 0;
+    // this variable will be used to syncronize different thread-blocks.
+    unsigned int *retirement_count;
 
     // t_start = get_time_msec();
     // first, allocate memory for ALL the images. The allocated memory size should be in the size
@@ -299,13 +374,25 @@ double gpu_large_average_distance_calculator(uchar *images1, uchar *images2) {
     // each pair of images has its own histogram, so we need to allocate memory enough for N_IMG_PAIRS
     CUDA_CHECK( cudaMalloc(((void**)&gpu_hist1), (HIST_SIZE * N_IMG_PAIRS) * sizeof(int)) );
     CUDA_CHECK( cudaMalloc(((void**)&gpu_hist2), (HIST_SIZE * N_IMG_PAIRS) * sizeof(int)) );
-    // allocate memory for the histogram distance result.
+    // allocate memory for the histogram distances result.
+    // this time we are using multiple thread blocks calculation at the same time, each one has its own
+    // histogram distance. We need an array big enough to save all these distances. There are total of 
+    // N_IMG_PAIRS distances, but we also need a padding of 240 to have a power of 2 (for the reduce).
+    // So this should be the size of the the array allocated.
+    CUDA_CHECK( cudaMalloc(((void**)&gpu_hist_distances), N_IMG_PAIRS_POW2 * sizeof(double)) );
+    // allocate memory for the FINAL result which is the sum of all partial sums
     CUDA_CHECK( cudaMalloc(((void**)&gpu_hist_distance), sizeof(double)) );
+    // allocate memory for the sync counter.
+    CUDA_CHECK( cudaMalloc(((void**)&retirement_count), sizeof(unsigned int)) );
 
     // now we need to copy the images from Host to Device.
     // this might take some time because we are copying a pretty "heavy" bundle of data.
     CUDA_CHECK( cudaMemcpy(gpu_image1, images1, ((IMG_DIMENSION * IMG_DIMENSION) * N_IMG_PAIRS * sizeof(uchar)), cudaMemcpyHostToDevice) );
     CUDA_CHECK( cudaMemcpy(gpu_image2, images2, ((IMG_DIMENSION * IMG_DIMENSION) * N_IMG_PAIRS * sizeof(uchar)), cudaMemcpyHostToDevice) );
+    // intitalize all the array with zeros so the padding will be zeros.
+    CUDA_CHECK( cudaMemset(gpu_hist_distances, 0, N_IMG_PAIRS_POW2 * sizeof(double)) );
+    // initialize the sync count to 0.
+    CUDA_CHECK( cudaMemset(retirement_count, 0, sizeof(int)) );
 
     // after all those allocations, calculate the images histograms using the "large" version of 
     // image_to_hisogram. This version is doing the calculation on all the images at once, using
@@ -316,10 +403,15 @@ double gpu_large_average_distance_calculator(uchar *images1, uchar *images2) {
     CUDA_CHECK( cudaDeviceSynchronize() );
     // calculate the distance of each pair of images using 256 threads (as the size of the histogram - one for each element)
     // for each block. We need N_IMG_PAIRS blocks for N_IMG_PAIRS image-pairs.
-    histogram_distance_large<<<N_IMG_PAIRS, 256>>>(gpu_hist1, gpu_hist2, gpu_hist_distance);
-    // that's it, from now on everyting is more or less similar to the other "gpu_average_distance_calculator"
-    // wait for histogram distance calculation to finish.
+    histogram_distance_large<<<N_IMG_PAIRS, 256>>>(gpu_hist1, gpu_hist2, gpu_hist_distances);
+    // wait for partial histogram distances calculation to finish.
     CUDA_CHECK( cudaDeviceSynchronize() );
+    // reduce the histograms distances. We use 512 threads in each block to sum 1024 elements. we need a total of 
+    // ((size of array) / (threads in block)) threadblocks.
+    hist_reduce_large<<<(N_IMG_PAIRS_POW2 / 1024), 512>>>(gpu_hist_distances, retirement_count, gpu_hist_distance); 
+    // wait for the total distance calculation to finish.
+    CUDA_CHECK( cudaDeviceSynchronize() );
+    // that's it, we are finally done. From now on everyting is more or less similar to the other "gpu_average_distance_calculator"
     // copy the distance back from the gpu to the cpu.
     CUDA_CHECK( cudaMemcpy(&cpu_total_distance, gpu_hist_distance, sizeof(double), cudaMemcpyDeviceToHost) );
     
@@ -328,7 +420,9 @@ double gpu_large_average_distance_calculator(uchar *images1, uchar *images2) {
     CUDA_CHECK( cudaFree(gpu_image2) );
     CUDA_CHECK( cudaFree(gpu_hist1) );
     CUDA_CHECK( cudaFree(gpu_hist2) );
+    CUDA_CHECK( cudaFree(gpu_hist_distances) );
     CUDA_CHECK( cudaFree(gpu_hist_distance) );
+    CUDA_CHECK( cudaFree(retirement_count) );
 
     CUDA_CHECK( cudaDeviceSynchronize() );
     double average_distance = cpu_total_distance / N_IMG_PAIRS;
